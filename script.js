20a8c95394f5ac3a373d2ea11568f4a14c3c0c67
JacopoLombardi <lombajacopo@gmail.com>
2024-02-19 15:27:52 +0100

    [add] password stampata in console

---
+++ script.js
@@ -0,0 +1,8 @@
+const firstName = prompt('Inserisci il tuo Nome');
+const lastName = prompt('Inserisci il tuo Cognome');
+const favouriteColor = prompt('Inserisci il tuo Colore Preferito');
+const number = 24;
+ 
+const passwordGenerated = firstName + lastName + favouriteColor + number;
+
+console.log(passwordGenerated);
